b941478eef6892a5bd23e627d8e5686d511488ad
kayleesatch <kayleesatch@gmail.com>
2025-11-11 14:39:44 -0700

    still adjusting the hero

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -7,7 +7,7 @@ export default function Hero() {
         <section className="text-center py-20">
             <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-4 mx-8 text-slate-600 shadow-white-lg">Need a Website?<br />I would love to make one for you!</h1>
             <h1 className="text-5xl font-bold mb-4 text-yellow-300 shadow-white-lg">Hi, I'm Kaylynn</h1>
-            <p className="text-2xl font-semibold px-8 text-purple-600">Freelance Web Developer & Designer</p>
+            <p className="text-2xl font-bold px-8 text-purple-600">Freelance Web Developer & Designer</p>
             <p className="text-xl px-12">Take a look and see what works best for you and if you have any questions just reach out and
                 <a 
                     onClick={() => setOpen(true)}
